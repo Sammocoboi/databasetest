bdfb14e2eda0c58321a039f89cd434206f669878
Sammo <80219847+Sammocoboi@users.noreply.github.com>
2026-01-21 10:01:18 +0500

    create pytest models product
    create pytest models product

--- config/product/tests/test_models.py
+++ config/product/tests/test_models.py
@@ -1,1 +1,13 @@
 import pytest 
+from product.models import Product
+
+# тест 1 проверяем создание продукта 
+def test_product_creation():
+    product = Product.objects.create(
+        name = "Apples",
+        price = 999.99,
+        in_stoct = True
+    )
+    assert product.name == "Apples"
+    assert product.price == 999.99
+    assert product.in_stock == True
