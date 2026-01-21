5b008e7c58e14e1f1dea9f6b7e718af412a189cf
Sammo <80219847+Sammocoboi@users.noreply.github.com>
2026-01-21 10:21:12 +0500

    pytest product passed
    pytest product passed

--- config/product/tests/test_models.py
+++ config/product/tests/test_models.py
@@ -2,12 +2,14 @@ import pytest
 from product.models import Product
 
 # тест 1 проверяем создание продукта 
+@pytest.mark.django_db
 def test_product_creation():
     product = Product.objects.create(
         name = "Apples",
         price = 999.99,
-        in_stoct = True
+        in_stock = True
     )
     assert product.name == "Apples"
     assert product.price == 999.99
     assert product.in_stock == True
+    
